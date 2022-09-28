dbf9335d6dae9208ede81a20bbdbcc2345d69b0a
paotra9 <102278733+paotra9@users.noreply.github.com>
2022-09-28 01:33:57 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/itics/p47t2120222/PrincipalActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.itics.p47t2120222;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
